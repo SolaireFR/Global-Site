8a26efc2f5007b38d4ff519f19fd6936510513b2
SolaireFR <jesappellegrootdu59@gmail.com>
2024-04-20 11:01:42 +0200

    initial commit

---
+++ src/main/java/perso/fr/globalsite/GlobalSiteApplication.java
@@ -0,0 +1,13 @@
+package perso.fr.globalsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/perso/fr/globalsite/GlobalSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package perso.fr.globalsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
