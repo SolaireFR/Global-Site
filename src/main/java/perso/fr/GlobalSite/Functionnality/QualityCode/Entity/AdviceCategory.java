0ab9dd4e781b94e221798f31bb5be251529f22f2
MorganSecretin <secretin.morgan@gmail.com>
2024-06-26 01:38:56 +0200

    Correction bug proprietes bdd

--- src/main/java/perso/fr/GlobalSite/Functionnality/QualityCode/Entity/AdviceCategory.java
+++ src/main/java/perso/fr/GlobalSite/Functionnality/QualityCode/Entity/AdviceCategory.java
@@ -1,19 +1,10 @@
 package perso.fr.GlobalSite.Functionnality.QualityCode.Entity;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -33,24 +24,4 @@ public class AdviceCategory {
 
     @Column(nullable=false)
     private String name;
-
-    @ManyToMany(mappedBy = "categories")
-    private List<Advice> advices = new ArrayList<>();
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "parent_id") // nom de la colonne dans la base de données
-    private AdviceCategory parentCategory;
-
-    @OneToMany(mappedBy = "parentCategory", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<AdviceCategory> subCategories = new ArrayList<>();
-
-    public void addSubCategory(AdviceCategory subCategory) {
-        subCategories.add(subCategory);
-        subCategory.setParentCategory(this);
-    }
-
-    public void removeSubCategory(AdviceCategory subCategory) {
-        subCategories.remove(subCategory);
-        subCategory.setParentCategory(null);
-    }
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO user (email, password, enabled) VALUES ('user@example.com', 'password', true);
-INSERT INTO role (id, name) VALUES (1, 'ROLE_USER');
-INSERT INTO users_roles (user_id, role_id) VALUES (1, 1);
+INSERT INTO users (creation, display_name, email, is_enabled, last_connection, locked, password) VALUES ('2024-05-18 00:52:11', 'jesappellegrootdu59', 'jesappellegrootdu59@gmail.com', 0, NULL, 0, '$2a$10$XAI6YZSJAIOpaQ6Cb4rgkubM5E8zoIvCUT4LRK3gw4fRKbXAIVhSy'), ('2024-06-12 20:47:39', 'admin', 'admin@test.com', 1, NULL, 0, '$2a$10$XAI6YZSJAIOpaQ6Cb4rgkubM5E8zoIvCUT4LRK3gw4fRKbXAIVhSy');
+INSERT INTO roles (name) VALUES ('ROLE_USER'), ('ROLE_ADMIN');
+INSERT INTO users_roles (user_id, role_id) VALUES (1, 1), (2, 2);
