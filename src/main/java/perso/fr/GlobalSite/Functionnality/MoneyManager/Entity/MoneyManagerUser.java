9f1fa0026e0f2162bd7bec11577ec4530f01097b
m.secretin <m.secretin@iesi.info>
2024-08-14 09:23:46 +0200

    Ajout version requise dans checkstyle et check de MoneyManagerUser.

--- src/main/java/perso/fr/GlobalSite/Functionnality/MoneyManager/Entity/MoneyManagerUser.java
+++ src/main/java/perso/fr/GlobalSite/Functionnality/MoneyManager/Entity/MoneyManagerUser.java
@@ -13,12 +13,13 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import perso.fr.GlobalSite.Main.Entity.UserData;
 
+/** Classe Entity User de MoneyManagerUser.*/
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name="money_manager_users")
+@Table(name = "money_manager_users")
 public class MoneyManagerUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,9 +29,11 @@ public class MoneyManagerUser {
     @JoinColumn(name = "user_data_id", referencedColumnName = "id")
     private UserData userData;
 
+    /** Constructeur de MoneyManagerUser permettant l'ajout de userData.
+     *
+     * @param userData L'entité regroupant les fonctionnalités de l'utilisateur.
+     */
     public MoneyManagerUser(UserData userData) {
         this.userData = userData;
     }
-
-    
 }
